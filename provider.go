1be9a5f7dc954384175c627d0cfc835b7ea22313
Dmitrijs Zubriks <dima.zubrik@gmail.com>
2022-06-17 00:38:32 +0300

    Refactor core provider code

--- provider.go
+++ provider.go
@@ -1,59 +1,73 @@
 package main
 
 import (
-	"github.com/hashicorp/terraform/helper/schema"
-	"github.com/hashicorp/terraform/terraform"
+	"ansoni/terraform-provider-scalyr/resource/monitor"
+	"context"
 	scalyr "github.com/ansoni/terraform-provider-scalyr/scalyr-go"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
-func Provider() terraform.ResourceProvider {
+const (
+	RegionEnvArg           = "SCALYR_REGION"
+	ServerEnvArg           = "SCALYR_SERVER"
+	ReadLogTokenEvnArg     = "SCALYR_READLOG_TOKEN"
+	WriteLogTokenEnvArg    = "SCALYR_WRITELOG_TOKEN"
+	ReadConfigTokenEnvArg  = "SCALYR_READCONFIG_TOKEN"
+	WriteConfigTokenEnvArg = "SCALYR_WRITECONFIG_TOKEN"
+	TeamEnvArg             = "SCALYR_TEAM"
+)
+
+func CreateProvider() *schema.Provider {
 	return &schema.Provider{
 		Schema: map[string]*schema.Schema{
-			"region": &schema.Schema{Type: schema.TypeString,
+			"region": {
+				Type:        schema.TypeString,
 				Optional:    true,
-				DefaultFunc: schema.EnvDefaultFunc("SCALYR_REGION", "us"),
+				DefaultFunc: schema.EnvDefaultFunc(RegionEnvArg, "us"),
 				Description: "Scalyr Region",
 			},
-			"endpoint": &schema.Schema{
+			"endpoint": {
 				Type:        schema.TypeString,
 				Optional:    true,
-				DefaultFunc: schema.EnvDefaultFunc("SCALYR_SERVER", nil),
+				DefaultFunc: schema.EnvDefaultFunc(ServerEnvArg, nil),
 				Description: "Scalyr Server",
 			},
-			"read_log_token": &schema.Schema{
+			"read_log_token": {
 				Type:        schema.TypeString,
 				Optional:    true,
-				DefaultFunc: schema.EnvDefaultFunc("SCALYR_READLOG_TOKEN", nil),
+				DefaultFunc: schema.EnvDefaultFunc(ReadLogTokenEvnArg, nil),
 				Description: "Scalyr ReadLog API Token",
 			},
-			"write_log_token": &schema.Schema{
+			"write_log_token": {
 				Type:        schema.TypeString,
 				Optional:    true,
-				DefaultFunc: schema.EnvDefaultFunc("SCALYR_WRITELOG_TOKEN", nil),
+				DefaultFunc: schema.EnvDefaultFunc(WriteLogTokenEnvArg, nil),
 				Description: "Scalyr WriteLog API Token",
 			},
-			"read_config_token": &schema.Schema{
+			"read_config_token": {
 				Type:        schema.TypeString,
 				Optional:    true,
-				DefaultFunc: schema.EnvDefaultFunc("SCALYR_READCONFIG_TOKEN", nil),
+				DefaultFunc: schema.EnvDefaultFunc(ReadConfigTokenEnvArg, nil),
 				Description: "Scalyr ReadConfig API Token",
 			},
-			"write_config_token": &schema.Schema{
+			"write_config_token": {
 				Type:        schema.TypeString,
 				Optional:    true,
-				DefaultFunc: schema.EnvDefaultFunc("SCALYR_WRITECONFIG_TOKEN", nil),
+				DefaultFunc: schema.EnvDefaultFunc(WriteConfigTokenEnvArg, nil),
 				Description: "Scalyr WriteConfig API Token",
 			},
-			"team": &schema.Schema{
+			"team": {
 				Type:        schema.TypeString,
 				Optional:    true,
-				DefaultFunc: schema.EnvDefaultFunc("SCALYR_TEAM", nil),
+				DefaultFunc: schema.EnvDefaultFunc(TeamEnvArg, nil),
 				Description: "Scalyr Team Identifier",
 			},
 		},
 		ResourcesMap: map[string]*schema.Resource{
-			"scalyr_event": resourceEvent(),
-			"scalyr_file":  resourceFile(),
+			"scalyr_event":   resourceEvent(),
+			"scalyr_file":    resourceFile(),
+			"scalyr_monitor": monitor.Resource(),
 		},
 		DataSourcesMap: map[string]*schema.Resource{
 			"scalyr_file":   datasourceFile(),
@@ -61,18 +75,38 @@ func Provider() terraform.ResourceProvider {
 			"scalyr_teams":  datasourceTeams(),
 			"scalyr_tokens": datasourceTokens(),
 		},
-		ConfigureFunc: providerConfigure,
+		ConfigureContextFunc: providerConfigure,
 	}
 }
 
-func providerConfigure(d *schema.ResourceData) (interface{}, error) {
+func providerConfigure(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
+	team := d.Get("team").(string)
 	region := d.Get("region").(string)
 	endpoint := d.Get("endpoint").(string)
 	readLogToken := d.Get("read_log_token").(string)
 	writeLogToken := d.Get("write_log_token").(string)
 	readConfigToken := d.Get("read_config_token").(string)
 	writeConfigToken := d.Get("write_config_token").(string)
-	tokens := scalyr.ScalyrTokens{ReadLog: readLogToken, WriteLog: writeLogToken, ReadConfig: readConfigToken, WriteConfig: writeConfigToken}
-	team := d.Get("team").(string)
-	return scalyr.NewClient(&scalyr.ScalyrConfig{Endpoint: endpoint, Region: region, Team: team, Tokens: tokens})
+
+	tokens := scalyr.ScalyrTokens{
+		ReadLog:     readLogToken,
+		WriteLog:    writeLogToken,
+		ReadConfig:  readConfigToken,
+		WriteConfig: writeConfigToken,
+	}
+
+	var diagnostics diag.Diagnostics
+
+	client, err := scalyr.NewClient(&scalyr.ScalyrConfig{
+		Endpoint: endpoint,
+		Region:   region,
+		Team:     team,
+		Tokens:   tokens,
+	})
+
+	if err != nil {
+		return nil, diag.FromErr(err)
+	}
+
+	return client, diagnostics
 }
